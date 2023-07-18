ce625ae910cd17ad13f3f6ac3915c9cba6975cf4
Anuraj Suman <anuras92@zedat.fu-berlin.de>
2023-07-18 10:12:31 +0200

    added readme and few changes

--- src/services/BezService.js
+++ src/services/BezService.js
@@ -4,10 +4,6 @@ const getAll = () => {
   return http.get("http://127.0.0.1:5000/bezirksgrenzens");
 };
 
-const get = (id) => {
-  return http.get(`/tutorials/${id}`);
-};
-
 const create = (data) => {
   return http.post("http://127.0.0.1:5000/bezirksgrenzen", data);
 };
@@ -20,22 +16,13 @@ const remove = (id) => {
   return http.delete(`http://127.0.0.1:5000/bezirksgrenzen/${id}`);
 };
 
-const removeAll = () => {
-  return http.delete(`/tutorials`);
-};
 
-const findByTitle = (title) => {
-  return http.get(`/tutorials?title=${title}`);
-};
 
 const BezService = {
   getAll,
-  get,
   create,
   update,
   remove,
-  removeAll,
-  findByTitle,
 };
 
 export default BezService;
